37d599b1ba45ec783af9db7d8e3eb26d20541a12
aquino-genesis <aquino.gm@obmontessori.edu.ph>
2026-09-04 11:12:17 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# === BASIC DATA TYPES ===
+nameOne = "Genesis Aquino" # string
+nameTwo = "Arturo Santos" # string
+nameThree = "Jack Sabitsana" # string
+
+ageOne = 15 # integer
+ageTwo = 15 # integer
+ageThree = 15 # integer
+
+heightOne = "1500cm" # string
+heightTwo = "-10cm" # string
+heightThree = "10000cm" # string
+
+student_type_one = False # boolean
+student_type_two = False # boolean
+student_type_three = False # boolean
+
+# === CONTAINER TYPES ===
+
+# List
+ourCountries = ["Venezuela", "Israel"]
+
+# Dictionary
+ourItems = {
+    "car_brand": "Lamborghini", #string
+    "shoe_size": 7.0, # float
+    "best_friend": "OBMC Tweaks", # string
+}
+
+print("joke")
+ourItems["best_friend"] = "Arturo & Jack & John ICT"
+
+# List
+ourFruitList = {"Banana", "Strawberry", "Tomato", "Mango", "Lychee"}
+
+# Tuple
+ourWeek = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
